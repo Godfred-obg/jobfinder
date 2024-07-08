aad5204bc2143fe398d815adc9ddb3c556200cf0
Godfred-obg <130106494+Godfred-obg@users.noreply.github.com>
2024-07-08 16:13:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const app = express();
 const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
+require("dotenv").config();
+const pool = require("../db");
+const { hashPassword, comparePassword } = require("/auth");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const { sendRegistrationEmail } = require("/email");
+
+app.use(express.json({ extended: false }));
+app.use(
+  cors({
+    origin: "*", // Replace with the origin of your React app
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+const isNullOrEmpty = (value) =>
+  !value || value.toLowerCase() === "undefined" || value === "";
+
+//app.use(express.json()); //req.body
+app.use(cookieParser());
+app.use("/files", express.static("files"));
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./files");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/product", product);
 

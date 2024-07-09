fc8047cc822fa950b279e5c43c5d6ae05388537d
Godfred-obg <130106494+Godfred-obg@users.noreply.github.com>
2024-07-09 06:16:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 const pool = require("./db");
 const { hashPassword, comparePassword } = require("./auth");
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 //const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const { sendRegistrationEmail } = require("./email");

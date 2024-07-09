fde41cfb22fab059beddc7a1821e33d1a4c57998
Godfred-obg <130106494+Godfred-obg@users.noreply.github.com>
2024-07-08 17:14:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const pool = require("./db");
 const { hashPassword, comparePassword } = require("./auth");
 //const cookieParser = require("cookie-parser");
 //const jwt = require("jsonwebtoken");
-/*const fs = require("fs");
-const { sendRegistrationEmail } = require("./email");
+const fs = require("fs");
+/*const { sendRegistrationEmail } = require("./email");
 
 app.use(express.json({ extended: false }));*/
 app.use(

57a2f810dea10c7f84ea9092f194e20e3ae1160f
Godfred-obg <130106494+Godfred-obg@users.noreply.github.com>
2024-07-09 09:24:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,16 +28,7 @@ const isNullOrEmpty = (value) =>
 
 app.use(cookieParser());
 app.use("/files", express.static("files"));
-const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./files");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null, uniqueSuffix + file.originalname);
-  },
-});
+
 
 const upload = multer({ storage: storage });
 
